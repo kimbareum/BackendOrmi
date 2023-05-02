f4cc930932b02b30da1a131f9c9ae80180cd3953
bareum.kim <131732610+kimbareum@users.noreply.github.com>
2023-05-02 17:05:30 +0900

    Add files via upload

---
+++ 20230501_python1/20230502/연습문제_거스름돈반환.py
@@ -0,0 +1,19 @@
+while True:
+    try:
+        money = int(input("금액을 입력해 주세요. "))
+    except:
+        print("숫자만 입력해주세요.")
+        continue
+    if money>=0:
+        break
+    else:
+        print("0 이상의 수를 입력해주세요.") 
+money_type = (5000,1000,500,100)
+money_num = [0,0,0,0]
+for i in range(4):
+    money_num[i] = money//money_type[i]
+    # money-=money_type[i]*money_num[i]
+    money %= money_type[i]
+
+print(f'5000원:{money_num[0]}장, 1000원:{money_num[1]}장,',
+        f'500원:{money_num[2]}개, 100원:{money_num[3]}개')
